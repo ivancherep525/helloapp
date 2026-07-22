0c51aa54028d7e34c52c66474463691c23b28c97
black <black@example.com>
2026-07-22 20:19:03 +0300

    Add function calls and docstring based on review feedback

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+def greetWhite():
+    print("hello white")
+
 def greetBlack():
+    """Prints 'hello black' greeting."""
     print("hello black")
+
+# Основной код
+print("hello everyone")
+greetWhite()
+greetBlack()
